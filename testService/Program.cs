1eebf05111780d2c23a663939d4a8254860bac5e
User <User@WIN-LISK69P3BGP>
2022-10-09 15:01:15 +0400

    Add project files.

---
+++ testService/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace testService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+
+                x.Service<TestService>(s =>
+                {
+                    s.ConstructUsing(testService => new TestService());
+                    s.WhenStarted(testService => testService.Start());
+                    s.WhenStopped(testService => testService.Stop());
+
+                });
+                x.RunAsLocalSystem();
+                x.SetServiceName("TestService");
+                x.SetDisplayName(" Test Service");
+                x.SetDescription("This service is for test");
+            });
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());   
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ testService/TestService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace testService
+{
+    public class TestService
+    {
+        private readonly Timer _timer;
+       
+
+        public TestService()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += Timer_Ellapsed;
+        }
+
+        private void Timer_Ellapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Users\User\Desktop\New folder\hello.txt", lines);
+        }
+        public void Start()
+        {
+            _timer.Start();
+        }
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
